53c580e9aa6bca25bcbf5522ffd8ca90675975f4
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 02:23:12 +0900

    Fix: Wrap schedule items in Dismissible for deletion
    Wraps each schedule item in a Dismissible widget to enable swipe-to-delete functionality.

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -82,7 +82,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
                       final schedule = snapshot.data![index];
 
-                      
+                      return Dismissible(key: key, child: child)
                     },
                   )
                 },
