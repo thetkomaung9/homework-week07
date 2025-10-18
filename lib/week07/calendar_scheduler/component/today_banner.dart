6ce1d1b8e953ffc52aa2ef4dfe3b83c95b5f8ca2
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 04:36:43 +0900

    feat(today_banner): Display month in TodayBanner
    The TodayBanner now displays the month in addition to the year.

--- lib/week07/calendar_scheduler/component/today_banner.dart
+++ lib/week07/calendar_scheduler/component/today_banner.dart
@@ -26,7 +26,7 @@ class TodayBanner  extends StatelessWidget{
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              '${selectedDate.year}'
+              '${selectedDate.year}년 ${selectedDate.month}'
             )
           ],
         ),
