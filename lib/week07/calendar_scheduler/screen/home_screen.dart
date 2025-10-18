392f30f86431d1ef8cf0f5198d3c2e6bac4a0b0c
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 01:55:12 +0900

    feat: Implement basic home screen layout
    Adds a basic home screen layout with a main calendar and schedule card components.

---
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -0,0 +1,4 @@
+import 'package:myapp/week07/calendar_scheduler/component/main_calendar.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/week07/calendar_scheduler/component/schedule_card.dart';
+import 'package:myapp/';
