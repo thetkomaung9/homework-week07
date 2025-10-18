1775230d218cfdde37ea192d6ac03c6bc8f0105d
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 02:46:12 +0900

    feat: Add CustomTextField component
    This commit introduces a basic CustomTextField component.

--- lib/week07/calendar_scheduler/component/custom_text_field.dart
+++ lib/week07/calendar_scheduler/component/custom_text_field.dart
@@ -2,3 +2,6 @@ import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+class CustomTextField extends StatelessWidget{
+  
+}
