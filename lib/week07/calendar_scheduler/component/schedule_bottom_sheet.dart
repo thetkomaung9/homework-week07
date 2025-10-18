86df37cac0c4d2b15c33aafcd1a393edff7e0a31
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 03:23:42 +0900

    feat: Implement ScheduleBottomSheet component
    This commit introduces the  component with basic UI elements and imports for future implementation.

---
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -0,0 +1,11 @@
+import 'package:myapp/week07/calendar_scheduler/component/custom_text_field.dart';
+import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
+import 'package:flutter/material.dart';
+
+import 'package:drift/drift.dart' hide Column;
+import 'package:get_it/get_it.dart';
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+
+class ScheduleBottomSheet extends StatefulWidget{
+  
+}
