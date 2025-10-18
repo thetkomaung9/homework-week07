883375d58e2ea5b60d95dc7d07461f5aea5839a6
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 01:49:42 +0900

    feat: Import necessary packages for calendar scheduler
    This commit adds imports for localization, drift database, and GetIt, setting up the foundation for the calendar scheduler feature.

--- lib/week07/calendar_scheduler/main.dart
+++ lib/week07/calendar_scheduler/main.dart
@@ -1,3 +1,6 @@
 import 'package:myapp/week07/calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/date';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+import 'package:get_it/get_it.dart';
+
