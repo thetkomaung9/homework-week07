ea73ec5c879fd6cd614c6dced91a13650c5f12a0
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 02:01:12 +0900

    feat: Implement basic HomeScreen widget
    This commit introduces the basic structure of the HomeScreen widget, which will serve as the main screen for the calendar scheduler application.

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -6,3 +6,12 @@ import 'package:myapp/week07/calendar_scheduler/component/schedule_bottom_sheet.
 import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
 import 'package:get_it/get_it.dart';
 import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+
+class HomeScreen extends StatefulWidget{
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+  
+}
