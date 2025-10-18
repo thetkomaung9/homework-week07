4231bc4541fceb8a5bf3ea97eaee9645bd3549ae
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 05:28:42 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -1,39 +1,43 @@
 import 'package:myapp/week07/calendar_scheduler/model/schedule.dart';
 import 'package:drift/drift.dart';
+
 import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as p;
+import 'package:path/path.dart';
 import 'dart:io';
+
 
 part 'drift_database.g.dart';
 
 @DriftDatabase(
-  tables: [Schedules],
+  tables:[
+    Schedules
+  ],
 )
-class LocalDatabase extends _$LocalDatabase {
+class LocalDatabase extends _$LocalDatabase{
   LocalDatabase() : super(_openConnection());
 
-  // Watch schedules for a specific date
-  Stream<List<Schedule>> watchSchedules(DateTime date) {
-    return (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
-  }
+  Stream<List<Schedule>> watchSchedules(DateTime data) =>
 
-  // Insert schedule
+  (select(schedules)..where((tbl) => tbl.date.equals(date))).watch();
+
   Future<int> createSchedule(SchedulesCompanion data) =>
-      into(schedules).insert(data);
+  into(schedules).insert(data);
 
-  // Delete schedule
-  Future<int> removeSchedule(int id) =>
-      (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
+Future<int> removeSchedule(int id) =>
+  (delete(schedules)..where((tbl) => tbl.id.equals(id))).go();
 
   @override
   int get schemaVersion => 1;
 }
 
-// Database connection
-LazyDatabase _openConnection() {
-  return LazyDatabase(() async {
-    final dbFolder = await getApplicationDocumentsDirectory();
+
+
+LazyDatabase _openConnection(){
+  return LazyDatabase(() async{
+
+
+    final dbFolder = await getApplicationCacheDirectory();
     final file = File(p.join(dbFolder.path, 'db.sqlite'));
     return NativeDatabase(file);
   });
