3d65ac7556d85ad8e5dbee872ae9554bf268365a
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 02:11:42 +0900

    feat(calendar): Add spacing below the calendar widget.

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -54,6 +54,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
               onDaySelected: OnDaySelected,
             ),
+            SizedBox(height: 8.0),
             
           ],
         ),
