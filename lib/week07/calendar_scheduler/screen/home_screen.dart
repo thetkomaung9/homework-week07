61f5bfd69c96d75dd120c75052a9d334601ef8a1
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 05:19:43 +0900

    Fix: Pass schedule.id to removeSchedule instead of Schedule.id
    The  function was being called with , which is undefined. This commit corrects it to pass the correct schedule's id by using .

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -89,7 +89,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         direction: DismissDirection.startToEnd,
 
                         onDismissed: (DismissDirection direction){
-                          GetIt.I<LocalDatabase>().removeSchedule(Schedule.id);
+                          GetIt.I<LocalDatabase>().removeSchedule(schedule.id);
                         },
                         child: Padding(
                           padding: const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
