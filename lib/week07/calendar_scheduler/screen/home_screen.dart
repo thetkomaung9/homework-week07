7d6147b4d1c0290354228deb94301f486217e986
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 05:13:42 +0900

    Updated Changes

--- lib/week07/calendar_scheduler/screen/home_screen.dart
+++ lib/week07/calendar_scheduler/screen/home_screen.dart
@@ -94,8 +94,8 @@ class _HomeScreenState extends State<HomeScreen> {
                         child: Padding(
                           padding: const EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
                           child: ScheduleCard(
-                            startTime: schedule.startTime,
-                            endTime: schedule.endTime,
+                            startTime: Schedules.startTime,
+                            endTime: Schedules.endTime,
                             content: Schedules.content,
                           ),
                         ),
