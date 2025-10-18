ec68a161d6c08639e337f687e5e05261c5ce4076
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 04:05:13 +0900

    feat(scheduler): Add content validator and hour range validation
    This commit adds a content validator to ensure schedule content is not empty and adds validation to the hour input to ensure it's within the 0-24 range.

--- lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
+++ lib/week07/calendar_scheduler/component/schedule_bottom_sheet.dart
@@ -135,5 +135,20 @@ class _ScheduleBottomSheetState extends State<ScheduleBottomSheet>{
     } catch (e) {
       return '숫자를 입력해주세요';
     }
+
+    if (number < 0 || number > 24){
+      return '0시부터 24시 사이를 입력해주세요';
+    }
+
+    return null;
+  }
+
+
+  String? contentValidator(String? val){
+    if(val == null || val.length == 0) {
+      return '값을 입력해주세요';
+    }
+
+    return null;
   }
 }
