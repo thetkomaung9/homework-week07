eb50492b0f82de9a3fad3f66eea7c9944b51459e
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 05:05:13 +0900

    Rename: Rename Schedule table to Schedules
    This commit renames the  table class to  for better pluralization and consistency.

--- lib/week07/calendar_scheduler/model/schedule.dart
+++ lib/week07/calendar_scheduler/model/schedule.dart
@@ -1,6 +1,6 @@
 import 'package:drift/drift.dart';
 
-class Schedule extends Table{
+class Schedules extends Table{
   IntColumn get id => integer().autoIncrement()();
   TextColumn get content => text()();
   DateTimeColumn get date => dateTime()();
