ec4400e07e6a9ebd2f9381bc8020a03fd6dba75b
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 04:41:43 +0900

    feat: Initialize Drift database setup
    Adds necessary imports, part directive, and DriftDatabase annotation to begin setting up the Drift database.

--- lib/week07/calendar_scheduler/database/drift_database.dart
+++ lib/week07/calendar_scheduler/database/drift_database.dart
@@ -2,4 +2,13 @@ import 'package:myapp/week07/calendar_scheduler/model/schedule.dart';
 import 'package:drift/drift.dart';
 
 import 'package:drift/native.dart';
-import 'package:pat';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'dart:io';
+
+
+part 'drift_database.g.dart';
+
+@DriftDatabase(
+  
+)
