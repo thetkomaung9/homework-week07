081143636a9007ee85874834fa5987387618a932
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 01:50:12 +0900

    feat: Implement basic main function
    Adds a basic  function to initialize the application.

--- lib/week07/calendar_scheduler/main.dart
+++ lib/week07/calendar_scheduler/main.dart
@@ -4,3 +4,8 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
 import 'package:get_it/get_it.dart';
 
+
+void main() async {
+
+  Widgets
+}
