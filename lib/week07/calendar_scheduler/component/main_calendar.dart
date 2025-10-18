b5e3e09d3cf6a4310291e69463ad70617ff115c8
thetko1996 <thetkomaung966@gmail.com>
2025-10-19 03:05:43 +0900

    feat: Implement basic MainCalendar widget
    This commit introduces a basic MainCalendar widget, which imports table_calendar and defines a StatelessWidget with an OnDaySelected callback.

--- lib/week07/calendar_scheduler/component/main_calendar.dart
+++ lib/week07/calendar_scheduler/component/main_calendar.dart
@@ -1,2 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
+
+class MainCalendar extends StatelessWidget{
+  final OnDaySelected onDaySelected;
+  
+}
